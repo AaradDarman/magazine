2689088dd884766feba237293eeee39c384c3632
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2021-06-22 10:57:52 +0430

    Add ThemeProvider To App.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,31 @@
 import React, { useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+
+import { lightTheme, darkTheme } from "../utils/themes";
+import { GlobalStyles } from "../utils/globalStyles";
+import { useDarkMode } from "../utils/useDarkMode";
+import MagazineSite from "./MagazineSite";
 
 const App = () => {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
 
-  return <MagazineSite />;
+  let themeMode = theme === "light" ? lightTheme : darkTheme;
+
+  useEffect(() => {
+      console.log('cha');
+    themeMode = theme === "light" ? lightTheme : darkTheme;
+  }, [theme]);
+
+  if (!componentMounted) {
+    return <div />;
+  }
+
+  return (
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <MagazineSite theme={theme} toggleTheme={toggleTheme} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
