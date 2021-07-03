a5bcd7558c75d735838341bd3f6b811d30ff2f5c
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2021-07-03 08:09:02 +0430

    Add HomeCarousel To MagazineSite.jsx

--- src/containers/MagazineSite.jsx
+++ src/containers/MagazineSite.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import HomeCarousel from "../components/HomeCarousel";
 import MainLayout from "../components/layouts/MainLayout";
 
 const MagazineSite = (props) => {
   return (
     <MainLayout {...props}>
+      <div style={{ marginTop: "40px" }}>
+        <HomeCarousel />
+      </div>
     </MainLayout>
   );
 };
