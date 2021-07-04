a7b985ec4b0acea3a2e340ca7d59d0f9a71d30be
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2021-07-04 12:35:13 +0430

    Create FeaturedPosts.jsx

---
+++ src/components/FeaturedPosts.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import styled from "styled-components";
+import { hexToRGBA } from "../utils/colorHelper";
+
+const Wraper = styled.section`
+  color: #fefefe;
+  .featured-posts-title {
+    background-color: ${({ theme }) => theme.accent};
+    padding: 0.5rem 0.7rem;
+    margin-bottom: 1rem;
+    border-radius: 0.2rem;
+  }
+  .featured-posts-item {
+    margin-bottom: 1rem;
+    position: relative;
+  }
+  .featured-posts-item::before {
+    z-index: 6;
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 200px;
+    width: 100%;
+    transition: 0.3s;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0) 0%,
+      #000000 70%
+    );
+    opacity: 0.85;
+    pointer-events: none;
+  }
+  .featured-posts-item a {
+    display: block;
+    width: 100%;
+    height: 300px;
+    background: url("images/banner.jpg") no-repeat center center;
+    background-size: cover;
+    position: relative;
+    min-height: 50px;
+    border-radius: 0.2rem;
+  }
+  .info {
+    z-index: 7;
+    position: absolute;
+    bottom: 0;
+  }
+  .info h3 {
+    font-size: 1.1rem;
+    background-color: ${({ theme }) => hexToRGBA(theme.accent, 0.8)};
+    transition: all 300ms ease-in-out;
+  }
+  .info p {
+    font-size: 0.9rem;
+  }
+  .info h3,
+  p {
+    padding: 8px 16px 8px 8px;
+  }
+  a:hover .info h3 {
+    background-color: ${({ theme }) => hexToRGBA(theme.accent, 1)};
+  }
+`;
+
+const FeaturedPosts = () => {
+  return (
+    <Wraper>
+      <div className="featured-posts-title">انتخاب سردبیر</div>
+      <div className="featured-posts-item">
+        <a href="#">
+          <div class="info w-100">
+            <h3 class="title mb-0">معرفی آیرون میدن – رقیب حسود بلک ویدو</h3>
+            <p class="sub-title">
+              همکار بلک ویدو که به خون ناتاشا رومانوف تشنه است
+            </p>
+          </div>
+        </a>
+      </div>
+      <div className="featured-posts-item">
+        <a href="#">
+          <div class="info w-100">
+            <h3 class="title mb-0">معرفی آیرون میدن – رقیب حسود بلک ویدو</h3>
+            <p class="sub-title">
+              همکار بلک ویدو که به خون ناتاشا رومانوف تشنه است
+            </p>
+          </div>
+        </a>
+      </div>
+      <div className="featured-posts-item">
+        <a href="#">
+          <div class="info w-100">
+            <h3 class="title mb-0">معرفی آیرون میدن – رقیب حسود بلک ویدو</h3>
+            <p class="sub-title">
+              همکار بلک ویدو که به خون ناتاشا رومانوف تشنه است
+            </p>
+          </div>
+        </a>
+      </div>
+    </Wraper>
+  );
+};
+
+export default FeaturedPosts;
